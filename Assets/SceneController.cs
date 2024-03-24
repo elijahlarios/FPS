518901e104e4de92e89d43d1950f5e63efe55892
aaronsuay <aaronsuay5@gmail.com>
2024-03-24 16:55:47 -0700

    Added Map/UI
    pathing needs work

--- Assets/SceneController.cs
+++ Assets/SceneController.cs
@@ -1,26 +1,39 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class SceneController : MonoBehaviour
 {
 
     [SerializeField] private GameObject enemyPrefab;
     private GameObject enemy;
+    public int rounds = 0;
+    public int enemyCount = 5;
+    public TextMeshProUGUI text;
     // Start is called before the first frame update
     void Start()
     {
         
     }
+    
 
     // Update is called once per frame
     void Update()
     {
+
+        //SetText
+        text.SetText("ROUND: " + rounds);
         if (enemy == null) {
-            enemy = Instantiate(enemyPrefab) as GameObject;
-            enemy.transform.position = new Vector3(0, 0.25f, 0);
-            float angle = Random.Range(0, 360);
-            enemy.transform.Rotate(0, angle, 0);
+            rounds++;
+            enemyCount = enemyCount + 2;
+            for(int i=0; i < enemyCount; i++){
+                enemy = Instantiate(enemyPrefab) as GameObject;
+                enemy.transform.position = new Vector3(0, 0, 0);
+                float angle = Random.Range(0, 360);
+                enemy.transform.Rotate(0, angle, 0);
+            }
+
         }
     }
 }
